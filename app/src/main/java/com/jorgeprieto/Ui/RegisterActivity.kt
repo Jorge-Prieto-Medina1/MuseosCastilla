ab8b1fa4c91dcf2abf6b464c387996b24923189d
Jorge <jorgepri00@gmail.com>
2021-05-06 02:02:44 +0200

    PrimeraParteLogin

---
+++ app/src/main/java/com/jorgeprieto/Ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.jorgeprieto.Ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jorgeprieto.museosjorgeprieto.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
